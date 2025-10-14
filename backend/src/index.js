f043ca7d48cf24aa5439c51584fe9b1bd1208245
MayankT10 <170873949+MayankT10@users.noreply.github.com>
2025-10-14 17:57:46 +0530

    Integrating Database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,18 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-  console.log("Sever is running on port 5001.");
+app.listen(PORT, () => {
+  console.log("Sever is running on PORT:" + PORT);
+  connectDB();
 });
