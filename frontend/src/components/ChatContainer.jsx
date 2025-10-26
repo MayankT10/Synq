cd5963f287fbbcb285fabfc3072d15e667411eda
MayankT10 <170873949+MayankT10@users.noreply.github.com>
2025-10-26 15:45:43 +0530

    Adding Chat Container

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,9 +1,28 @@
-import React from "react" 
+import React from "react";
+import { useChatStore } from "../store/useChatStore";
+import { useEffect } from "react";
+import ChatHeader from "./ChatHeader"
+import MessageInput from "./MessageInput"
 
 const ChatContainer = () => {
-    return (
-        <div>ChatContainer</div>
-    )
-}
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore();
 
-export default ChatContainer
+  if (isMessagesLoading) return <div>Loading...</div>;
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+
+      <p>messages...</p>
+
+      <MessageInput />
+    </div>
+  );
+};
+
+export default ChatContainer;
